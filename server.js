3518247cc7615106ead403aaf0ea5723b6eed8fa
Dan Calacci <dan@dcalacci.net>
2016-07-07 15:52:23 -0400

    refactor!

--- server.js
+++ server.js
@@ -1,51 +1,53 @@
 // Load required modules
-var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
-var io      = require("socket.io");         // web socket external module
-var easyrtc = require("easyrtc");           // EasyRTC external module
+const http = require('http')
+const express = require('express')
+const io = require('socket.io')
+const easyrtc = require('easyrtc')
+const path = require('path')
 
-// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
-var httpApp = express();
-httpApp.use(express.static(__dirname + "/static/"));
+var app = express()
 
-// Start Express http server on port 8080
-var webServer = http.createServer(httpApp).listen(process.env.PORT);
+app.use(express.static(path.join(__dirname, '/public/')))
+
+let webServer = http.createServer(app).listen(process.env.PORT)
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = io.listen(webServer, {"log level":1});
+var socketServer = io.listen(webServer, {'log level': 1})
 
-easyrtc.setOption("logLevel", "debug");
+easyrtc.setOption('logLevel', 'debug')
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
-        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
-            callback(err, connectionObj);
-            return;
-        }
+easyrtc.events.on('easyrtcAuth', function (socket, easyrtcid, msg, socketCallback, callback) {
+  easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
+    if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+      callback(err, connectionObj)
+      return
+    }
 
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+    connectionObj.setField('credential', msg.msgData.credential, {'isShared': false})
 
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+    console.log('[' + easyrtcid + '] Credential saved!', connectionObj.getFieldValueSync('credential'))
 
-        callback(err, connectionObj);
-    });
-});
+    callback(err, connectionObj)
+  })
+})
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
-    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
-});
-
+easyrtc.events.on('roomJoin', function (connectionObj, roomName, roomParameter, callback) {
+  console.log('[' + connectionObj.getEasyrtcid() + '] Credential retrieved!', connectionObj.getFieldValueSync('credential'))
+  easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback)
+})
 
 // Start EasyRTC server
-var rtc = easyrtc.listen(httpApp, socketServer, null, function(err, rtcRef) {
-    console.log("Initiated");
+easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
+  if (err) {
+    console.log('err:', err)
+  }
 
-    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
-        console.log("roomCreate fired! Trying to create: " + roomName);
+  console.log('Initiated')
 
-        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
-    });
-});
+  rtcRef.events.on('roomCreate', function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    console.log('roomCreate fired! Trying to create: ' + roomName)
+    appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback)
+  })
+})
