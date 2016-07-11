a73bdfb72f0695e7347292472cf1b2823d1e9702
Dan Calacci <dan@dcalacci.net>
2016-07-11 13:19:00 -0400

    refactoring client-side code!

---
+++ client/audio.js
@@ -0,0 +1,50 @@
+/* global easyrtc AudioContext*/
+const $ = require('jQuery')
+
+function processAudio () {
+  console.log('preparing to process audio...')
+  var aCtx = new AudioContext()
+  var analyser = aCtx.createAnalyser()
+  var whosVideo = $('#box0')
+  var whosTalking = $('#box0Talk')
+  var volumeDuration = []
+  var audioSource = aCtx.createMediaStreamSource(easyrtc.getLocalStream())
+  audioSource.connect(analyser)
+  setInterval(function () {
+    // get the average, bincount is fftsize / 2
+    var array = new Uint8Array(analyser.frequencyBinCount)
+    analyser.getByteFrequencyData(array)
+    var volume = getAverageVolume(array)
+    var threshold = 20
+
+    if (volume > threshold) {
+      whosTalking.innerHTML = "you're talking!"
+      volumeDuration.push(whosVideo.currentTime)
+    } else {
+      whosTalking.innerHTML = 'you are not talking!'
+      if (volumeDuration.length > 0) {
+        console.log('last talk duration: ' + (Math.max(...volumeDuration) - Math.min(...volumeDuration)))
+        volumeDuration = []
+      }
+    }
+    console.log('vol:' + volume) // here's the volume
+  }, 1000)
+}
+
+function getAverageVolume (array) {
+  var values = 0
+  var average
+  var length = array.length
+
+  // get all the frequency amplitudes
+  for (var i = 0; i < length; i++) {
+    values += array[i]
+  }
+
+  average = values / length
+  return average
+}
+
+module.exports = {
+  startProcessingAudio: processAudio
+}
---
+++ client/rtcClient.js
@@ -0,0 +1,95 @@
+/* global easyrtc location prompt*/
+const $ = require('jQuery')
+const _ = require('lodash')
+const utils = require('./utils')
+const audio = require('./audio')
+
+var $scope = {
+  roomName: null,
+  needToCallOtherUsers: null
+}
+
+function callEverybodyElse (roomName, userList, selfInfo) {
+  if ($scope.needToCallOtherUsers) {
+    _.forEach(userList, (rtcId) => {
+      easyrtc.call(
+        rtcId,
+        function success (otherCaller, mediaType) {
+          console.log('success', otherCaller, mediaType)
+        },
+        function failure (errorCode, errorMessage) {
+          console.log('failure', errorCode, errorMessage)
+        }
+      )
+    })
+    $scope.needToCallOtherUsers = false
+  }
+}
+
+function loginSuccess () {
+  console.log('login successful')
+  $('#box0').on('playing', function () {
+    console.log('user box is playing...')
+    audio.startProcessingAudio()
+  })
+}
+
+function getIdOfBox (boxNum) {
+  return '#box' + boxNum
+}
+
+function init () {
+  console.log('initializing RTC client...')
+  easyrtc.dontAddCloseButtons()
+  easyrtc.setRoomEntryListener(function (entry, roomName) {
+    console.log('entered room!')
+    $scope.needToCallOtherUsers = true
+  })
+  easyrtc.setRoomOccupantListener(callEverybodyElse)
+  easyrtc.easyApp('rhythm.party',
+                  'box0',
+                  ['box1', 'box2', 'box3', 'box4'],
+                  loginSuccess)
+  joinRoom()
+
+  easyrtc.setDisconnectListener(function () {
+    easyrtc.showError('LOST-CONNECTION', 'Lost connection to signaling server')
+  })
+  easyrtc.setOnCall(function (easyrtcid, slot) {
+    console.log('getConnection count=' + easyrtc.getConnectionCount())
+    $(getIdOfBox(slot + 1)).css('visibility', 'visible')
+  })
+  easyrtc.setOnHangup(function (easyrtcid, slot) {
+    setTimeout(function () {
+      $(getIdOfBox(slot + 1)).css('visibility', 'hidden')
+    }, 20)
+  })
+
+  $('#leaveRoomLink').click(function () {
+    // call roomLeave handler
+    easyrtc.leaveRoom($scope.roomName, function () {
+      location.assign(location.href.substring(0, location.href.indexOf('?')))
+    })
+  })
+}
+
+function joinRoom () {
+  $scope.roomName = utils.getParam('room')
+  if ($scope.roomName === null || $scope.roomName === '' || $scope.roomName === 'null') {
+    $scope.roomName = prompt('enter room name:')
+    if (location.href.indexOf('?room=') === -1) {
+      location.assign(location.href + '?room=' + $scope.roomName)
+    } else {
+      location.assign(location.href + $scope.roomName)
+    }
+  } else {
+    easyrtc.joinRoom($scope.roomName)
+    console.log('entered room: ' + $scope.roomName)
+    $('#roomIndicator').html("Currently in room '" + $scope.roomName + "'")
+    $('#leaveRoomLink').css('visibility', 'visible')
+  }
+}
+
+module.exports = {
+  initClient: init
+}
