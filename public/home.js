5e4d25564e9e0343148b1bc1d52ca45c65bf6bdf
Sarah Aladetan <sarahaladetan@gmail.com>
2016-07-14 14:52:03 -0400

    added cool webcam background effect

--- public/home.js
+++ public/home.js
@@ -2,3 +2,19 @@ $('#quickRoom').on('click', function () {
   var randomRoom = Math.floor(Math.random() * (Math.pow(2, 30) - 1 + 1)) + 1
   window.location.href = '/index.html?room=' + randomRoom
 })
+
+var video = document.querySelector('#bgvid')
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({video: true}, handleVideo, videoError)
+}
+
+function handleVideo (stream) {
+  video.src = window.URL.createObjectURL(stream)
+}
+
+function videoError (e) {
+    $('#bgvid').css('visibility', 'visible')
+}
