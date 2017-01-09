858db5cfac43b149cf5e73cb5e9c79f40b4f7bf0
Dan Calacci <dan@dcalacci.net>
2017-01-09 11:49:39 -0500

    I don't know how to use middleware

--- server.js
+++ server.js
@@ -15,23 +15,8 @@ browserify.settings({
 })
 
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200)
-  }
-  else {
-    next()
-  }
-}
-
 browserify.settings.development('basedir', __dirname)
-app.use(cors)
-app.use(allowCrossDomain)
+app.use(cors())
 app.get('/js/main.js', browserify('./client/main.js'))
 app.use(express.static(path.join(__dirname, '/public/')))
 
